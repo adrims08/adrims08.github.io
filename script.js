f01fb1e6fb04e27cd9dfe0eb3b1e7293295baa86
Adriana M. Sánchez <83677068+adrims08@users.noreply.github.com>
2024-04-20 15:43:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// El código de JavaScript es únicamente para ocultar el menú
+
+
+
+
+let menuVisible = false;
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible=false;
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible=true;
+    }
+}
